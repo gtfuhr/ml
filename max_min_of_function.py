32d77451a1263c34ea456d00c44576e4c5408293
Gabriel Tobias Fuhr <gt.fuhr@gmail.com>
2019-09-06 11:28:50 -0300

    Add max_min of a function using derivatives

---
+++ max_min_of_function.py
@@ -0,0 +1,31 @@
+from sympy import Symbol, solve, Derivative
+x = Symbol('x')
+f = x**5 - 30*x**3 + 50*x
+d1 = Derivative(f, x).doit()
+
+critical_points = solve(d1)
+print(critical_points)
+
+A = critical_points[2]
+B = critical_points[1]
+C = critical_points[1]
+D = critical_points[3]
+
+'''
+    Because all the critical points for this function lie within the considered
+    interval, they are all relevant for our search for the global maximum and
+    minimum of f(x). We may now apply the so-called second derivative test to
+    narrow down which critical points could be global maxima or minima.
+    First, we calculate the second-order derivative for the function f(x).
+    Note that to do so, we enter 2 as the third argument:
+
+    Excerpt from the book Doing Math with Python
+'''
+
+d2 = Derivative(f, x, 2).doit()
+
+
+print(d2.subs({x:B}).evalf())
+print(d2.subs({x:C}).evalf())
+print(d2.subs({x:A}).evalf())
+print(d2.subs({x:D}).evalf())
