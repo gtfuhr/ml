ac30e941b9fcd6f6bed731b69478dd415b2facb3
Gabriel Tobias Fuhr <gt.fuhr@gmail.com>
2019-09-08 21:28:27 -0300

    Add another limit example

--- math/calculus/limits_of_a_function.py
+++ math/calculus/limits_of_a_function.py
@@ -6,8 +6,11 @@ Limit(1/x, x, S.Infinity)
 
 l = Limit(1/x, x, S.Infinity)
 # Using Limit to solve, guess what? Limits!
-print(l.doit())
+# print(l.doit())
 
-print(Limit(1/x, x, 0, dir='-').doit())
+# print(Limit(1/x, x, 0, dir='-').doit())
 
-print(Limit(sin(x)/x, x, 0).doit())
+# print(Limit(sin(x)/x, x, 0).doit())
+lim = Limit((x**3 + 3*x**2 -x -3)/(x**3 + -x**2 +2), x, -1)
+# print(help(lim))
+print(lim.doit(hints=True))
