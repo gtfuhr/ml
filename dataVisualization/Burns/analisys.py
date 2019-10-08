3b5543f15bd1f21b047ff387bca8c3553aad489e
Gabriel Tobias Fuhr <gt.fuhr@gmail.com>
2019-10-08 14:39:27 -0300

    Add Brazil Burns datasets from opendata and pre-analisys

---
+++ dataVisualization/Burns/analisys.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Data attribs
+# Index(['Código CNUC', 'Nome da UC', 'Categoria da UC: sigla federal',
+#        'Categoria da UC: nomenclatura nacional', 'Grupo de Proteção',
+#        'Ano de criação', 'Coordenação Regional do ICMBio',
+#        'Área estimada da UC (ha)', 'Bioma referencial',
+#        'Área queimada em 2018', 'Área queimada em 2017',
+#        'Área queimada em 2016', 'Área queimada em 2015',
+#        'Área queimada em 2014', 'Área queimada em 2013',
+#        'Área queimada em 2012'],
+def main():
+    data = pd.read_csv("DIMIF-queima.csv",delimiter=";")
+    areas_hec = data['Área estimada da UC (ha)']
+    areas_queimada = data['Área queimada em 2016']
+
+    print(areas_queimada)    
+
+if __name__ == "__main__":
+    main()
